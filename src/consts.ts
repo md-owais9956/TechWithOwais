5079dbfdce3995439ff0b641883208bd01ea4405
MOHD OWAIS <md.owais111234@gmail.com>
2025-10-22 23:02:36 +0530

    ----

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Tech With Owais';
-export const SITE_DESCRIPTION = 'By Owais, a student developer. Building projects, learning in public, and teaching tech.'
+export const SITE_DESCRIPTION = 'By Owais, a student developer. Building projects, learning in public, and teaching tech.';
